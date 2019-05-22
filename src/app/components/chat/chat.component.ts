502309f25a142b678060387c0ac7b15ccee3bad0
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-22 21:24:03 +0200

    displaying  messages from firebase

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -15,6 +15,12 @@ export class ChatComponent {
   }
 
   sendMessage() {
+    if (this.message.length === 0) {
+      return;
+    }
+    this.chatService.addMessage(this.message)
+      .then(() => this.message = '')
+    .catch ((err) => console.error('Error sending message', err));
 
   }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,23 +1,40 @@
 import { Injectable } from '@angular/core';
 
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Message } from '../interfaces/message.interface';
+
+import { map } from 'rxjs/operators';
+import { text } from '@angular/core/src/render3';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  private itemsCollection: AngularFirestoreCollection<any>;
-  
-  public chats: any[] = [];
-  
+  private itemsCollection: AngularFirestoreCollection<Message>;
+  public chats: Message[] = [];
+
   constructor(private afs: AngularFirestore) {
 
 
   }
 
-  loadMessages(){
-    this.itemsCollection = this.afs.collection<any>('chats');
-    return  this.itemsCollection.valueChanges();
+  loadMessages() {
+    this.itemsCollection = this.afs.collection<Message>('chats');
+    return this.itemsCollection.valueChanges().pipe(
+      map((mensajes: Message[]) => {
+        this.chats = mensajes;
+      }));
+  }
+
+  addMessage(text: string) {
+    const message: Message = {
+      name: 'Demo',
+      message: text,
+      date: new Date().getTime()
+    };
+
+    return this.itemsCollection.add(message);
+
   }
 }
