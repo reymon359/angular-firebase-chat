d2e8911b069f9059be6e9c7668dd372446706099
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-22 21:40:46 +0200

    loading messages and scrolling to bottom

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChatService } from '../../services/chat.service';
 
 @Component({
@@ -6,20 +6,26 @@ import { ChatService } from '../../services/chat.service';
   templateUrl: './chat.component.html',
   styles: []
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
   message = '';
+  element: any
   constructor(public chatService: ChatService) {
-    this.chatService.loadMessages().subscribe((messages: any[]) => {
-      console.log(messages);
-    })
-  }
+    this.chatService.loadMessages().subscribe(() => {
+      setTimeout(() => {
 
+        this.element.scrollTop = this.element.scrollHeight;
+      }, 20);
+    });
+  }
+  ngOnInit() {
+    this.element = document.getElementById('app-messages');
+  }
   sendMessage() {
     if (this.message.length === 0) {
       return;
     }
     this.chatService.addMessage(this.message)
       .then(() => this.message = '')
-    .catch ((err) => console.error('Error sending message', err));
+      .catch((err) => console.error('Error sending message', err));
   }
 }
